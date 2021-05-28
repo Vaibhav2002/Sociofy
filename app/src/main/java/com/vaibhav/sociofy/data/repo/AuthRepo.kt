030993f33e66309eb1bc8eb627dff69fb7833799
Vaibhav2002 <vaibhavsam2511@gmail.com>
2021-05-28 17:38:17 +0530

    first commit

---
+++ app/src/main/java/com/vaibhav/sociofy/data/repo/AuthRepo.kt
@@ -0,0 +1,4 @@
+package com.vaibhav.sociofy.data.repo
+
+class AuthRepo {
+}
--- app/src/main/java/com/vaibhav/sociofy/data/repository/Preferences.kt
+++ app/src/main/java/com/vaibhav/sociofy/data/repository/Preferences.kt
@@ -3,6 +3,8 @@ package com.vaibhav.sociofy.data.repository
 import android.content.Context
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatDelegate
+import com.google.gson.Gson
+import com.vaibhav.sociofy.data.models.User
 import com.vaibhav.sociofy.util.Constants
 import com.vaibhav.sociofy.util.checkDarkMode
 import javax.inject.Inject
@@ -31,4 +33,15 @@ class Preferences @Inject constructor(
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
     else
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+
+    fun saveUserData(user: User) {
+        val userData = Gson().toJson(user)
+        sharedPref.edit().putString("USER", userData).apply()
+    }
+
+    fun getUserData(): User {
+        val userString = sharedPref.getString("USER", null);
+        return Gson().fromJson(userString, User::class.java)
+    }
 }
